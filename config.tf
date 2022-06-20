312c785a34a1decd025cb62b0e88fa262bb54023
SaiBhargav205 <33156323+SaiBhargav205@users.noreply.github.com>
2022-06-20 19:46:10 +0530

    Create config.tf

---
+++ config.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.18.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "app_server1" {
+  ami           = "ami-079b5e5b3971bd10d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance1"
+  }
+}
